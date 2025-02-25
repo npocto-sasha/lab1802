efff45a8f4f44ec1c32c9019fafac6f1b86fc317
Александр <53083162+npocto-sasha@users.noreply.github.com>
2025-02-25 17:06:51 +0900

    FIX 3

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
    message = 'Добро пожаловать!'
-   url = 'https://icon-icons.com/icons2/2428/PNG/512/github_black_logo_icon_147128.png'
+   url = 'static/github_black_logo_icon_147128.png'
    return render_template('index.html', message=message, url=url)
