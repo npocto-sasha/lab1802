470b62757d6ea9bba9f00684a804f22752d89923
Александр <53083162+npocto-sasha@users.noreply.github.com>
2025-02-25 16:48:39 +0900

    Image FIX

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
    message = 'Добро пожаловать!'
-   url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fgithub-logo_25231&psig=AOvVaw3mjTCC5S3CILtwV1zvffkx&ust=1740554781613000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCOil-f6l3osDFQAAAAAdAAAAABAE'
+   url = 'https://icon-icons.com/icons2/2428/PNG/512/github_black_logo_icon_147128.png'
    return render_template('index.html', message=message, url=url)
